2b63af7fc46fa92dca5983cb6ad9ceae64c5a7ce
prathamesh <prathameshchavan5145gmail.com>
2024-05-31 21:54:40 +0530

    asga

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2-instance" {
     ami = "ami-0f58b397bc5c1f2e8"
     instance_type = "t2.micro"
     key_name = "lallya"
-    vpc_security_group_ids = "sg-0aaa73e6b0db66be2"  
+    vpc_security_group_ids = "sg-0aaa73e6b0db66be2"
     tags = {
       Name = "janhavi"
     }
