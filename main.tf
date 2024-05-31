d16a025223879b42eff80e35c8701dcc1fcea59f
prathamesh <prathameshchavan5145gmail.com>
2024-05-31 22:11:36 +0530

    asthoa

--- main.tf
+++ main.tf
@@ -11,3 +11,29 @@ resource "aws_instance" "ec2-instance" {
       Name = "janhavi"
     }
 }
+resource "aws_security_group" "sg" {
+    vpc_id = "vpc-03b2fa3150bf67007"
+    description = "al"
+    ingress {
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "TCP"
+        from_port = 80
+        to_port = 80
+    }
+    egress {
+        cidr_blocks = ["0.0.0.0/0"]
+        protocol = "TCP"
+        from_port = 80
+        to_port = 80
+    }
+}
+
+resource "aws_instance" "ec2-instance1" {
+    ami = "ami-0f58b397bc5c1f2e8"
+    instance_type = "t2.micro"
+    key_name = "lallya"
+    vpc_security_group_ids = [aws_security_group.sg.id]
+    tags = {
+      Name = "janhavi2"
+    }
+}
